a9552c7e31b4c1cbbb95e9f5c955da3abe9a06bf
Aaron Sarkissian <aaronsarkissian@gmail.com>
2018-09-01 16:06:24 +0400

    hardcode links for publications

--- src/components/Publications/Publications.js
+++ src/components/Publications/Publications.js
@@ -43,31 +43,31 @@ class Publications extends Component {
           <CardView
             headline={'Oligarchies and Strategic Danger to Small State Security'}
             body={'Sample Summary'}
-            link={this.state.pdfs[0]}
+            link={'https://firebasestorage.googleapis.com/v0/b/regional-kinetics-dd.appspot.com/o/Publications%2FOligarchies-and-Strategic-Danger-to-Small-State-Security.pdf?alt=media&token=e39ac68d-daed-46f0-a910-ac95db574924'}
           />
 
           <CardView
             headline={'Returns on Diplomatic Investment Zionist Policies and the Armenians'}
             body={'Sample Summary'}
-            link={this.state.pdfs[1]}
+            link={'https://firebasestorage.googleapis.com/v0/b/regional-kinetics-dd.appspot.com/o/Publications%2FReturns-on-Diplomatic-Investment-Zionist-Policies-and-the-Armenians.pdf?alt=media&token=3ae50c6f-4206-45ac-ad9e-49a5239174bf'}
           />
 
           <CardView
             headline={'The Challenge of the Indigenous Arms Industry'}
             body={'Sample Summary'}
-            link={this.state.pdfs[2]}
+            link={'https://firebasestorage.googleapis.com/v0/b/regional-kinetics-dd.appspot.com/o/Publications%2FThe-Challenge-of-the-Indigenous-Arms-Industry.pdf?alt=media&token=2ac74a1e-69dd-49f1-964d-b07c4444bdb4'}
           />
 
           <CardView
             headline={'The Programmable Diplomatic Kill Switch'}
             body={'Sample Summary'}
-            link={this.state.pdfs[3]}
+            link={'https://firebasestorage.googleapis.com/v0/b/regional-kinetics-dd.appspot.com/o/Publications%2FThe-Programmable-Diplomatic-Kill-Switch.pdf?alt=media&token=8c86e9f6-aeb0-4566-b873-6c1ceffeabd7'}
           />
 
           <CardView
             headline={'Why Azerbaijan is unable to offer viable solutions to the Nagorno-Karabakh Conflict'}
             body={'Sample Summary'}
-            link={this.state.pdfs[4]}
+            link={'https://firebasestorage.googleapis.com/v0/b/regional-kinetics-dd.appspot.com/o/Publications%2FWhy-Azerbaijan-is-unable-to-offer-viable-solutions-to-the-Nagorno-Karabakh-Conflict.pdf?alt=media&token=4fc2f218-847a-4e4e-836d-d33fec36193f'}
           />
         </ul>
       </div>
