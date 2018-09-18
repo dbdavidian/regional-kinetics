a695193f540ca32acfd0926b1d0544fe37244b86
dbdavidian <42601284+dbdavidian@users.noreply.github.com>
2018-09-18 12:10:31 +0400

    Added summaries

--- src/components/Publications/Publications.js
+++ src/components/Publications/Publications.js
@@ -42,36 +42,36 @@ class Publications extends Component {
         <ul className="flexcontainer">
           <CardView
             headline={'The Curious Treaty of Kars'}
-            body={'Sample Summary'}
+            body={'Perhaps treaties, such as Kars, are established to be broken and their tenets ignored'}
             link={'https://firebasestorage.googleapis.com/v0/b/regional-kinetics-dd.appspot.com/o/Publications%2FThe-Curious-Treaty-of-Kars.pdf?alt=media&token=7dd77a36-df04-40e9-9260-45faab5f53b6'}
           />
           <CardView
             headline={'Oligarchies and Strategic Danger to Small State Security'}
-            body={'Sample Summary'}
+            body={'The term oligarchy has taken on fresh interpretations since the fall of the Soviet Union and a generation of relaxed controls on western capital'}
             link={'https://firebasestorage.googleapis.com/v0/b/regional-kinetics-dd.appspot.com/o/Publications%2FOligarchies-and-Strategic-Danger-to-Small-State-Security.pdf?alt=media&token=e39ac68d-daed-46f0-a910-ac95db574924'}
           />
 
           <CardView
             headline={'Returns on Diplomatic Investment Zionist Policies and the Armenians'}
-            body={'Sample Summary'}
+            body={'There were repercussions to Zionist pursuits associated with the Armenian Question over a century ago'}
             link={'https://firebasestorage.googleapis.com/v0/b/regional-kinetics-dd.appspot.com/o/Publications%2FReturns-on-Diplomatic-Investment-Zionist-Policies-and-the-Armenians.pdf?alt=media&token=3ae50c6f-4206-45ac-ad9e-49a5239174bf'}
           />
 
           <CardView
             headline={'The Challenge of the Indigenous Arms Industry'}
-            body={'Sample Summary'}
+            body={' would be remiss of established states not to maximize their indigenous defense capabilities'}
             link={'https://firebasestorage.googleapis.com/v0/b/regional-kinetics-dd.appspot.com/o/Publications%2FThe-Challenge-of-the-Indigenous-Arms-Industry.pdf?alt=media&token=2ac74a1e-69dd-49f1-964d-b07c4444bdb4'}
           />
 
           <CardView
             headline={'The Programmable Diplomatic Kill Switch'}
-            body={'Sample Summary'}
+            body={'War is a mere continuation of policy with other means'}
             link={'https://firebasestorage.googleapis.com/v0/b/regional-kinetics-dd.appspot.com/o/Publications%2FThe-Programmable-Diplomatic-Kill-Switch.pdf?alt=media&token=8c86e9f6-aeb0-4566-b873-6c1ceffeabd7'}
           />
 
           <CardView
             headline={'Why Azerbaijan is unable to offer viable solutions to the Nagorno-Karabakh Conflict'}
-            body={'Sample Summary'}
+            body={'Azerbaijan has cornered itself with the inability to offer peaceful solutions to the Nagorno-Karabakh (NK) conflict'}
             link={'https://firebasestorage.googleapis.com/v0/b/regional-kinetics-dd.appspot.com/o/Publications%2FWhy-Azerbaijan-is-unable-to-offer-viable-solutions-to-the-Nagorno-Karabakh-Conflict.pdf?alt=media&token=4fc2f218-847a-4e4e-836d-d33fec36193f'}
           />
         </ul>
