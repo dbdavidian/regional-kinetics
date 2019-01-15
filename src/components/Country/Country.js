f7794d762e98db95263558b8826f6a5715fcf3fc
dbdavidian <42601284+dbdavidian@users.noreply.github.com>
2019-01-15 18:34:37 +0400

    Fixed spellig error in Israeli Policy Brief

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -138,7 +138,7 @@ const Country = (props) => {
           <CardView
             headline={'Full Recognition of the Armenian Genocide is in the National Interest of the State of Israel'}
             body={'Policy Brief'}
-            link={'https://firebasestorage.googleapis.com/v0/b/regional-kinetics-dd.appspot.com/o/Recommendations%2FIsrael%2FPosition_Paper--Israeli_Recognition_of_the_Armenian_Genocide_01-2019.pdf?alt=media&token=14d10881-9070-4002-8e92-3de59db9487b'}
+            link={'https://firebasestorage.googleapis.com/v0/b/regional-kinetics-dd.appspot.com/o/Recommendations%2FIsrael%2FPosition_Paper--Israeli_Recognition_of_the_Armenian_Genocide_01-2019.pdf?alt=media&token=57540a20-0092-417a-a803-d585fb6c95d1'}
           />
         </ul>
       </div>
