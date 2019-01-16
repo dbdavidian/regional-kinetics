3bdc95f30e9270e2ffccf4866397c5be75d2e71a
dbdavidian <42601284+dbdavidian@users.noreply.github.com>
2019-01-16 22:09:03 +0400

    Removed the comma in the Israeli brief date

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -138,7 +138,7 @@ const Country = (props) => {
           <CardView
             headline={'Full Recognition of the Armenian Genocide is in the National Interest of the State of Israel'}
             body={'Policy Brief'}
-            link={'https://firebasestorage.googleapis.com/v0/b/regional-kinetics-dd.appspot.com/o/Recommendations%2FIsrael%2FPosition_Paper--Israeli_Recognition_of_the_Armenian_Genocide_01-2019.pdf?alt=media&token=57540a20-0092-417a-a803-d585fb6c95d1'}
+            link={'https://firebasestorage.googleapis.com/v0/b/regional-kinetics-dd.appspot.com/o/Recommendations%2FIsrael%2FPosition_Paper--Israeli_Recognition_of_the_Armenian_Genocide_01-2019.pdf?alt=media&token=288dbe44-4fc1-4b9f-b07f-30e2c45630e6'}
           />
         </ul>
       </div>
