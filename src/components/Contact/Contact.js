85bbc6ee7140523de03138a0fbd24a71d7752cdd
Aaron Sarkissian <aaronsarkissian@gmail.com>
2019-02-19 12:44:45 +0400

    Add mailchimp

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,12 +7,69 @@ const Contact = () => (
   <div className="container">
     <Helmet>
       <title>Contact</title>
-      <meta name="description" content="contact"></meta>
+      <meta name="description" content="contact" />
       <link rel="canonical" href="https://www.regionalkinetics.com/contact" />
     </Helmet>
     <h1>Contact</h1>
     <p>Email: regionalkinetics@gmail.com</p>
-    <a href="mailto:regionalkinetics@gmail.com" target="_top">Send Mail</a>
+    <a href="mailto:regionalkinetics@gmail.com" target="_top">
+      Send Mail
+    </a>
+    <div id="mc_embed_signup">
+      <form
+        action="https://regionalkinetics.us20.list-manage.com/subscribe/post?u=cd6af7cb0d72bd1758415260b&amp;id=ce4d6834b6"
+        method="post"
+        id="mc-embedded-subscribe-form"
+        name="mc-embedded-subscribe-form"
+        class="validate"
+        target="_blank"
+        novalidate
+      >
+        <div id="mc_embed_signup_scroll">
+          <h2>Subscribe to our mailing list</h2>
+          <div class="mc-field-group">
+            <label for="mce-EMAIL">Email Address </label>
+            <input
+              type="email"
+              name="EMAIL"
+              class="required email"
+              id="mce-EMAIL"
+            />
+          </div>
+          <div id="mce-responses" class="clear" />
+          <div
+            class="response"
+            id="mce-error-response"
+            style={{ display: "none" }}
+          />
+          <div
+            class="response"
+            id="mce-success-response"
+            style={{ display: "none" }}
+          />
+          <div
+            style={{ position: "absolute", left: "-5000px" }}
+            aria-hidden="true"
+          >
+            <input
+              type="text"
+              name="b_cd6af7cb0d72bd1758415260b_ce4d6834b6"
+              tabindex="-1"
+              value=""
+            />
+          </div>
+          <div class="clear">
+            <input
+              type="submit"
+              value="Subscribe"
+              name="subscribe"
+              id="mc-embedded-subscribe"
+              class="button"
+            />
+          </div>
+        </div>
+      </form>
+    </div>
   </div>
 );
 
